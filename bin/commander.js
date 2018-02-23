6a6ff3bacadcaff48ddc26caf40e7472b87c536f
MAGICSCIENTISS <magicscientistggg@gmail.com>
2018-02-23 15:00:42 +0800

    fix commander bug

--- bin/commander.js
+++ bin/commander.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-
+let path = require('path')
 let program = require('commander');
 let uglifySite = require("../uglifyBuild.js");
 
 //set commander opt
-program.version('0.1.0')
-    .option('--c, --configPath <n>', '设置配置文件路径')
+program.version('0.1.3')
+    .option('-c, --configPath <n>', '设置配置文件路径')
     .option('-m, --modNames <n>', '设置希望执行的模块')
     .parse(process.argv);
 
@@ -13,12 +13,12 @@ program.version('0.1.0')
 console.log(program.configPath)
 console.log(program.modNames)
 
-let _cpath = program.configPath || './uglify.config.js';
+let _cpath = program.configPath || path.resolve("").replace(/\\/g,'/') + '/uglify.config.js';
 uglifySite.setConfig(_cpath)
     .then(res => {
         console.log(uglifySite.options);
-        uglifySite.start('common');
-        // uglifySite.start(program.modNames.split(','));
+        // uglifySite.start('common');
+        uglifySite.start(program.modNames.split(','));
     }).catch(err => {
         throw err;
     });
